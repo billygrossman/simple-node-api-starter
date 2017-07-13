bdfacbf6c35542b8f79571c7ad4e97d01ce296be
Billy Grossman <billy.grossman@gmail.com>
2017-07-13 12:30:12 -0400

    Update sample test

--- test/app.js
+++ test/app.js
@@ -1,11 +1,13 @@
 const request = require('supertest');
 const app = require('../app.js');
 
-describe('GET /up', () => {
+describe('Check that system is running', () => {
+
   it('should return 200 OK', (done) => {
     request(app)
-      .get('/login')
+      .get('/up')
       .expect(200, done);
   });
+
 });
 
